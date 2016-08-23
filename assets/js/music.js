b89a30c6e0fd28a057e86df2d5e32f3f8fbee0c1
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-08-23 00:37:59 -0400

    Added audio files and basic code to get the sounds working

---
+++ assets/js/music.js
@@ -0,0 +1,67 @@
+
+//<><><><><><>ALL SOUNDS IN THEIR BASIC FORM<><><><><><><><><><>//
+
+// // ARPEGIATOR
+// var arp = new Howl({ 
+//   src: ['assets/audio/arp-mm.mp3'],
+//   volume: 0.7,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// arp.play();
+
+// // BASS
+// var bass = new Howl({
+//   src: ['assets/audio/bass-mm.mp3'],
+//   volume: 1,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// bass.play();
+
+// // KICK DRUM
+// var kick = new Howl({
+//   src: ['assets/audio/kick-mm.mp3'],
+//   volume: 1,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// kick.play();
+
+// // SNARE DRUM
+// var snare = new Howl({
+//   src: ['assets/audio/snare-mm.mp3'],
+//   volume: 1,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// snare.play();
+
+// // PERCUSSION
+// var percussion = new Howl({
+//   src: ['assets/audio/percussion-mm.mp3'],
+//   volume: 1,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// percussion.play(); 
+
+// // HATS
+// var hats = new Howl({
+//   src: ['assets/audio/hats-mm.mp3'],
+//   volume: 0.5,
+//   pan:1.0,
+//   loop:true,
+//   html5:true,
+//   preload:true
+// });
+// hats.play();
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>//
+
+// var id1 = hats.play();
+
