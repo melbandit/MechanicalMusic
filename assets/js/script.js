e607296a4d24c979946e686774098c673f0a81d4
melbandit <melissa.i.sattler@gmail.com>
2016-08-25 21:50:44 -0400

    updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-
 Draggable.create("#knob",{
   type: "rotation",
   bounds:{minRotation:0, maxRotation:360}, //bounds, gives the rotation a max of 1, when on. unlimited when off
-  throwProps: true ,
+  throwProps: true,
   onDrag:Calc , 
   onThrowUpdate: Calc 
 });
 
 function Calc(){
-  console.log( this.rotation ) 
+ // console.log( this.rotation ) 
  var R = (this.rotation/360).toFixed(2) ;
  document.getElementById("myText").innerHTML= R ; 
 }
@@ -23,7 +22,7 @@ Draggable.create("#knob2",{
 });
 
 function Calc2(){
-  console.log( this.rotation ) 
+ // console.log( this.rotation ) 
  var S = (this.rotation/360).toFixed(2) ;
  document.getElementById("myText2").innerHTML= S ; 
 }
