e3c6b6095eeec1b3b7755fb68a02e0072a4ac0d9
melbandit <melissa.i.sattler@gmail.com>
2016-08-25 23:58:17 -0400

    did something to sound, fix back to normal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,92 @@
+
+// //<><><><><><>ALL SOUNDS IN THEIR BASIC FORM<><><><><><><><><><>//
+
+// KICK
+// var kick = new Howl({ 
+//   src: ['assets/audio/kick-mm.mp3'],
+//      html5:true,
+//      preload:true,
+//      loop:1,
+//          sprite:{
+//              kick:[0,10000,true]
+//          }
+// });
+// kick.play("kick");
+
+
+// // SNARE
+// var snare = new Howl({ 
+//   src: ['assets/audio/snare-mm.mp3'],
+    
+//      html5:true,
+//      preload:true,
+//      loop:1,
+//          sprite:{
+//              snare:[0,10000,true]
+//          }
+// });
+// snare.play("snare");
+
+// // HATS
+// var hats = new Howl({ 
+//   src: ['assets/audio/hats-mm.mp3'],
+//      html5:true,
+//      preload:true,
+//      volume:0.4,
+//      loop:1,
+//          sprite:{
+//              hats:[0,10000,true]
+//          }
+// });
+// hats.play("hats");
+
+// // PERCUSSION
+// var percussion = new Howl({ 
+//   src: ['assets/audio/percussion-mm.mp3'],
+//      html5:true,
+//      preload:true,
+//      loop:1,
+
+//          sprite:{
+//              percussion:[0,10000,true]
+//          }
+// });
+// percussion.play("percussion");
+
+// ARPEGIATOR
+// var arp = new Howl({ 
+//   src: ['assets/audio/arp-mm.mp3'],
+//     html5:true,
+//     preload:true,
+//     volume:0,
+//     loop:1,
+//         sprite:{
+//             arp:[0,10000,true]
+//         }
+// });
+// arp.play("arp");
+
+// BASS
+// var bass = new Howl({ 
+//   src: ['assets/audio/bass-mm.mp3'],
+//      html5:true,
+//      preload:true,
+//      loop:1,
+//          sprite:{
+//              bass:[0,10000,true]
+//          }
+// });
+// bass.play("bass");
+
+// //<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>//
+
+//Melissa Updating volume
+updateVolumeArp = function(value) {
+  //alert('before update volume:', arp.volume);
+  arp.volume = value;
+  //alert('after update volume:', arp.volume);
+};
+
 Draggable.create("#knob",{
     type: "rotation",
     bounds:{minRotation:0, maxRotation:360}, //bounds, gives the rotation a max of 1, when on. unlimited when off
@@ -27,3 +116,5 @@ function Calc2(){
     var S = (this.rotation/360).toFixed(2) ;
     document.getElementById("myText2").innerHTML= S ; 
 };
+
+
